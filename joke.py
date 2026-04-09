229f0d88062ef5d8dc8c0ce77bf9c26e9aefb8f9
phadeline <phadelinee@gmail.com>
2026-04-09 11:49:56 -0400

    first

--- joke.py
+++ joke.py
@@ -1,1 +1,6 @@
-import requests
+import requests 
+def get_joke():
+    url = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(url)
+    joke = response.json()
+    return f"{joke['setup']} - {joke['punchline']}"

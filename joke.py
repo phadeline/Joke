4d682e5a4fae2fb58538da04b97670d05a803e1f
phadeline <phadelinee@gmail.com>
2026-04-09 12:27:19 -0400

    joke retrieved

--- joke.py
+++ joke.py
@@ -1,3 +1,5 @@
 import requests
 
-r = requests.get('https://api.github.com/events')
+r = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json', 'User-Agent': 'My Library (https://github.com/phadeline/Joke)'})
+
+print(r.json()['joke']);
